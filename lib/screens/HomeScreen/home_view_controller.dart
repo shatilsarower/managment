f2ecb21b76ecba99e192a141dbc21a0acc2771a7
sms <shatild12@gmail.com>
2022-02-20 02:37:56 +0600

    lifesycle of some some getcontroller

--- lib/screens/HomeScreen/home_view_controller.dart
+++ lib/screens/HomeScreen/home_view_controller.dart
@@ -1,1 +1,21 @@
+import 'package:get/get.dart';
 
+class HomeViewController extends GetxController {
+  @override
+  void onInit() {
+    print("Class Name$runtimeType method name : onInit");
+    super.onInit();
+  }
+
+  @override
+  void onClose() {
+    print("Class Name$runtimeType method name : onClose");
+    super.onClose();
+  }
+
+  @override
+  void dispose() {
+    print("Class Name$runtimeType method name : dispose");
+    super.dispose();
+  }
+}
