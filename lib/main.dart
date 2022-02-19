4afb4cda87fa7a2853aaa817975fd40066f48ab9
sms <shatild12@gmail.com>
2022-02-20 02:15:14 +0600

    Change the name.

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData.dark(),
       initialRoute: '/',
-      home: const Home(),
+      home: const HomeView(),
     );
   }
 }
