6d7a3d689282596a835e3a8685e6c9fbbdbfc1e0
sms <shatild12@gmail.com>
2022-02-19 13:46:16 +0600

    adding the appbar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -5,6 +5,11 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('SMS'),
+      ),
+    );
   }
 }
